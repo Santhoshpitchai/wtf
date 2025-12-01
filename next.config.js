ac8d9da1a4b662159d17f9b4a40ec5224f038050
Santhoshpitchai <santhoshpitchai13@gmail.com>
2025-12-01 21:37:30 +0530

    Fix Vercel build - externalize puppeteer-core

--- next.config.js
+++ next.config.js
@@ -3,6 +3,20 @@ const nextConfig = {
   images: {
     domains: ['localhost'],
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Exclude chromium and puppeteer from webpack bundling
+      config.externals = [
+        ...(config.externals || []),
+        '@sparticuz/chromium',
+        'puppeteer-core'
+      ]
+    }
+    return config
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['puppeteer-core', '@sparticuz/chromium'],
+  },
 }
 
 module.exports = nextConfig
